62923a705aa44368e271025e6d8c4433795613b2
Victor Alcaraz <vctralcaraz@gmail.com>
2024-04-22 15:39:13 -0700

    v0.5 - added a lot of quality of life upgrades including autocomplete of host and folder

--- term_search.py
+++ term_search.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import streamlit.components.v1 as components
+from streamlit_tags import st_tags
 import ftputil
 import re
 
@@ -18,7 +19,6 @@ st.markdown("""
         <img style="width: 100px; margin-right: 15px;" src="https://pss-application-assets.s3.amazonaws.com/code-digger/code_digger-logo2.png" />
         <h1>Code Digger</h1>
     </div>
-    
     """, unsafe_allow_html=True)
 st.write("""
     This app helps search for the_field and the_sub_field ACF fields.
@@ -42,30 +42,93 @@ def term_search(FTP_HOST, FTP_USER, FTP_PASS, THEME_FOLDER):
                             files.append(f)
     return files
 
-form = st.form(key='my_form')
-FTP_HOST = form.text_input("FTP Host", key=None)
-FTP_USER = form.text_input("FTP User", key=None)
-FTP_PASS = form.text_input("FTP Password", type="password", key=None)
-THEME_FOLDER = form.text_input("Theme Folder", key=None)
-submit_button = form.form_submit_button("Submit")
-    
-if submit_button:
-    if FTP_HOST and FTP_USER and FTP_PASS and THEME_FOLDER:
-        st.markdown("""
-            <div style="display: flex; justify-content: center;">
+if 'FTP_HOST' not in st.session_state:
+    st.session_state['FTP_HOST'] = ''
+
+if 'FTP_USER' not in st.session_state:
+    st.session_state['FTP_USER'] = ''
+
+if 'FTP_PASS' not in st.session_state:
+    st.session_state['FTP_PASS'] = ''
+
+if 'THEME_FOLDER' not in st.session_state:
+    st.session_state['THEME_FOLDER'] = st.empty()
+
+if 'TF' not in st.session_state:
+    st.session_state['TF'] = st.empty()
+
+if 'results' not in st.session_state:
+    st.session_state['results'] = []
+
+if 'query_start' not in st.session_state:
+    st.session_state['query_start'] = False
+
+if 'query_complete' not in st.session_state:
+    st.session_state['query_complete'] = False
+
+# form = st.form(key='my_form')
+# FTP_HOST = form.text_input("FTP Host", key=None)
+# FTP_USER = form.text_input("FTP User", key=None)
+# FTP_PASS = form.text_input("FTP Password", type="password", key=None)
+# THEME_FOLDER = form.text_input("Theme Folder", key=None)
+# submit_button = form.form_submit_button("Submit")
+
+# form = st.form(key='my_form')
+# st.session_state['FTP_HOST'] = form.selectbox("FTP Host", options=['92.204.128.116', '92.204.139.144',  '92.204.139.241'], index=None, key=None)
+# st.session_state['FTP_USER'] = form.text_input("FTP User", key=None)
+# st.session_state['FTP_PASS'] = form.text_input("FTP Password", type="password", key=None)
+# # st.session_state['THEME_FOLDER'] = form.selectbox("Theme Folder", options=['pss-theme', 'other'], index=0, key=None)
+# st.session_state['THEME_FOLDER'] = st_tags(label='Theme Folder', text='Press Enter to add folder', suggestions=['pss-theme', 'click5-wp'], maxtags=1, key=None)
+
+with st.form('my_form'):
+    st.session_state['FTP_HOST'] = st_tags(label='FTP Host', text='Press Enter to add the host', suggestions=['92.204.128.116', '92.204.139.144',  '92.204.139.241'], maxtags=1, key=None)
+    st.session_state['FTP_USER'] = st.text_input('FTP User', key=None)
+    st.session_state['FTP_PASS'] = st.text_input('FTP Password', type="password", key=None)
+    st.session_state['THEME_FOLDER'] = st_tags(label='Theme Folder', text='Press Enter to add folder', suggestions=['pss-theme', 'click5-wp'], maxtags=1, key=None)
+
+    submit_button = st.form_submit_button("Submit")
+
+    if submit_button:
+        if st.session_state['FTP_HOST'] and st.session_state['FTP_USER'] and st.session_state['FTP_PASS'] and st.session_state['THEME_FOLDER']:
+            st.markdown("""
+                    <style>
+                        .miner {
+                            display: flex;
+                            justify-content: center;
+                        }
+                    </style>
+                """, unsafe_allow_html=True)
+            st.markdown("""
+                    <div class="miner">
+                        <img style="width: 470px; height: 75px; object-fit: cover;" src="https://pss-application-assets.s3.amazonaws.com/code-digger/miner.gif" />
+                    </div>
+                """, unsafe_allow_html=True)
+            with st.spinner('Working on your request...'):
+                st.session_state['results'] = term_search(st.session_state['FTP_HOST'][0], st.session_state['FTP_USER'], st.session_state['FTP_PASS'], st.session_state['THEME_FOLDER'][0])
+                st.session_state['query_complete'] = True
+
+            st.markdown("""
+                    <style>
+                        .miner {
+                            display: none;
+                        }
+                    </style>
+                """, unsafe_allow_html=True)
+        else:
+            st.error('Please fill all the fields before submitting!')
+
+if st.session_state['query_start'] is True and st.session_state['query_complete'] is False:
+    st.markdown("""
+            <div style="display: flex; justify-content: center;" class="miner">
                 <img style="width: 470px; height: 75px; object-fit: cover;" src="https://pss-application-assets.s3.amazonaws.com/code-digger/miner.gif" />
             </div>
-            """, unsafe_allow_html=True)
-        with st.spinner('Working on your request...'):
-            results = term_search(FTP_HOST, FTP_USER, FTP_PASS, THEME_FOLDER)
-            st.write('Search Results:')
-            st.divider()
-            if len(results) == 0:
-                st.write('There are no instances found...')
-            else:
-                st.write(len(results), 'were found')
-                for el in results:
-                    st.write(el)
+        """, unsafe_allow_html=True)
+if st.session_state['query_start'] is False and st.session_state['query_complete'] is True:
+    st.write('Search Results:')
+    st.divider()
+    if len(st.session_state['results']) == 0:
+        st.write('There are no instances found...')
     else:
-        st.error('Please fill all the fields before submitting!')
-
+        st.write(len(st.session_state['results']), 'were found')
+        for el in st.session_state['results']:
+            st.write(el)
